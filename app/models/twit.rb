14d8edd0cc0759c30d56bda898550159be3b109a
Eric Fontaine <ericfontaine13@gmail.com>
2016-09-01 19:48:30 +0200

    add twitter connection + get_tweets method

--- app/models/twit.rb
+++ app/models/twit.rb
@@ -1,6 +1,13 @@
 class Twit < ApplicationRecord
 
-  def self.most_liked()
+  def self.get_tweets user
+    all_tweets = CLIENT.user_timeline(user, count: "5", exclude_replies: true, include_rts: false)
+    all_tweets.each do |tweet|
+      Twit.create!(content: "#{tweet.text}", link: "#{tweet.uri}", like: "#{tweet.favorite_count}", retweet: "#{tweet.retweet_count}")
+    end
+  end
+
+  def self.most_liked
     order(like: :desc)
   end
 
---
+++ config/initializers/twitter_credentials.rb
@@ -0,0 +1,6 @@
+CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key        = '6mqMEmg4mymPb2nl1A1lnCfy2'
+  config.consumer_secret     = 'KyoCgiGE0LNV3NUhYf9pPAM38hdjwxqBcYiMKGpHu3rZdn3DLB'
+  config.access_token        = '751419255535665152-RwnSOHacbmSgiS5hH6mtc0RjDTrTcqD'
+  config.access_token_secret = 'jjR9Kedm6CWh2zurfIgy3UnasOzm49elh1vUzwmh2iyEs'
+end
